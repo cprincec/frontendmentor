6c6193a5f20d9591f8de46d6ca2893da5c1fb319
cprincec <chukwuprincec@gmail.com>
2023-06-29 17:59:58 +0100

    little animation

--- newsletter-sign-up-with-success-message-main/assets/js/success.js
+++ newsletter-sign-up-with-success-message-main/assets/js/success.js
@@ -1,2 +1,6 @@
 const span = document.querySelector(".email-value");
+const dismissBtn = document.querySelector("button");
 span.textContent = localStorage.getItem("email") || "defaultemail@gmail.com";
+dismissBtn.addEventListener("click", (e) => {
+  window.location.href = "../newsletter-sign-up-with-success-message-main";
+});
